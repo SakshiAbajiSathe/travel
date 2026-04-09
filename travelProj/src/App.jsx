d7edcf3e99f09441b6448a8be3dd2f0dfbfb46f9
Sakshi Patil <sp079716@gmail.com>
2026-04-09 04:47:36 +0000

    msg

--- travelProj/src/App.jsx
+++ travelProj/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Hello/>
 
       
       </>
