8b9f672b337020952775a5cf54c91142006aa5b8
Sakshi Sathe <sathesakshi0319@gmail.com>
2026-04-17 16:08:00 +0000

    route

--- travelProj/src/App.jsx
+++ travelProj/src/App.jsx
@@ -1,29 +1,77 @@
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import Testimonials from "./components/Testimonials";
+// import Subscribe from "./components/Subscribe";
+// import Destinations from "./components/Destination";
+// import Services from "./components/Services";
+// import TripCard from "./components/TripCard";
+// import Footer from "./components/Footer";
+
+
+
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Hero />
+//       <Services/>
+//       <Destinations/>
+//       <TripCard/>
+      
+//       <Testimonials/>
+//       <Subscribe/>
+//       <Footer/>
+//       </>
+//     );
+//   }
+
+// export default App;
+
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Destinations from "./components/Destinations";
+import TripCard from "./components/TripCard";
 import Testimonials from "./components/Testimonials";
 import Subscribe from "./components/Subscribe";
-import Destinations from "./components/Destination";
-import Services from "./components/Services";
-import TripCard from "./components/TripCard";
 import Footer from "./components/Footer";
+import  About  from "./components/About";
 
-
-
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Destinations />
+      <TripCard />
+      <Testimonials />
+      <Subscribe />
+      <About />
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Hero />
-      <Services/>
-      <Destinations/>
-      <TripCard/>
-      
-      <Testimonials/>
-      <Subscribe/>
-      <Footer/>
-      </>
-    );
-  }
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
--- travelProj/src/components/Navbar.jsx
+++ travelProj/src/components/Navbar.jsx
@@ -1,59 +1,108 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+// function Navbar() {
+//   const [menuOpen, setMenuOpen] = useState(false);
+
+//   return (
+//     <header className="flex justify-between items-center px-4 sm:px-6 md:px-10 lg:px-16 py-5">
+      
+      
+//       <h1 className="text-2xl sm:text-3xl font-bold">
+//         Jad<span className="text-yellow-500">oo</span>
+//       </h1>
+
+      
+//       <nav className="hidden md:flex gap-8 lg:gap-12 font-bold">
+//         <a href="#" className="hover:text-yellow-500 transition">Destinations</a>
+//         <a href="#" className="hover:text-yellow-500 transition">Hotels</a>
+//         <a href="#" className="hover:text-yellow-500 transition">Flights</a>
+//         <a href="#" className="hover:text-yellow-500 transition">Bookings</a>
+//       </nav>
+
+      
+//       <div className="flex items-center gap-3 sm:gap-4">
+        
+        
+//         <div className="hidden md:flex gap-4">
+//           <button className="font-bold hover:text-yellow-500">Login</button>
+//           <button className="border px-4 py-1 rounded font-bold hover:bg-yellow-500 hover:text-white transition">
+//             Sign up
+//           </button>
+//         </div>
+
+        
+//         <button
+//           className="md:hidden text-2xl"
+//           onClick={() => setMenuOpen(!menuOpen)}
+//         >
+//           ☰
+//         </button>
+//       </div>
+
+      
+//       {menuOpen && (
+//         <div className="absolute top-16 left-0 w-full bg-white shadow-md flex flex-col items-center gap-6 py-6 md:hidden z-50">
+          
+//           <a href="#" className="hover:text-yellow-500">Destinations</a>
+//           <a href="#" className="hover:text-yellow-500">Hotels</a>
+//           <a href="#" className="hover:text-yellow-500">Flights</a>
+//           <a href="#" className="hover:text-yellow-500">Bookings</a>
+
+//           <button className="font-bold hover:text-yellow-500">Login</button>
+//           <button className="border px-4 py-1 rounded font-bold hover:bg-yellow-500 hover:text-white transition">
+//             Sign up
+//           </button>
+//         </div>
+//       )}
+
+//     </header>
+//   );
+// }
+
+// export default Navbar;
+
+
+
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
-    <header className="flex justify-between items-center px-4 sm:px-6 md:px-10 lg:px-16 py-5">
-      
-      
-      <h1 className="text-2xl sm:text-3xl font-bold">
-        Jad<span className="text-yellow-500">oo</span>
+    <header className="flex justify-between items-center md:pr-16 px-6 md:px-16 py-5">
+
+      <h1 className="text-3xl font-bold">
+        <Link to="/">
+          Jad<span className="text-yellow-500">oo</span>
+        </Link>
       </h1>
 
-      
-      <nav className="hidden md:flex gap-8 lg:gap-12 font-bold">
-        <a href="#" className="hover:text-yellow-500 transition">Destinations</a>
-        <a href="#" className="hover:text-yellow-500 transition">Hotels</a>
-        <a href="#" className="hover:text-yellow-500 transition">Flights</a>
-        <a href="#" className="hover:text-yellow-500 transition">Bookings</a>
+      <nav className="hidden md:flex gap-10 p-5">
+        <Link to="/destinations" className="hover:text-yellow-500 font-bold">
+          Destinations
+        </Link>
+
+        <Link to="/services" className="hover:text-yellow-500 font-bold">
+          Services
+        </Link>
+
+        <Link to="/testimonials" className="hover:text-yellow-500 font-bold">
+          Testimonials
+        </Link>
+
+        <Link to="/" className="hover:text-yellow-500 font-bold">
+          Home
+        </Link>
+
+        <Link to="/about" className="hover:text-yellow-500 font-bold">
+          About
+        </Link>
       </nav>
 
-      
-      <div className="flex items-center gap-3 sm:gap-4">
-        
-        
-        <div className="hidden md:flex gap-4">
-          <button className="font-bold hover:text-yellow-500">Login</button>
-          <button className="border px-4 py-1 rounded font-bold hover:bg-yellow-500 hover:text-white transition">
-            Sign up
-          </button>
-        </div>
-
-        
-        <button
-          className="md:hidden text-2xl"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          ☰
+      <div className="flex gap-4">
+        <button className="font-bold hover:text-yellow-500">Login</button>
+        <button className="border px-4 py-1 rounded font-bold hover:bg-yellow-500 hover:text-white">
+          Sign up
         </button>
       </div>
-
-      
-      {menuOpen && (
-        <div className="absolute top-16 left-0 w-full bg-white shadow-md flex flex-col items-center gap-6 py-6 md:hidden z-50">
-          
-          <a href="#" className="hover:text-yellow-500">Destinations</a>
-          <a href="#" className="hover:text-yellow-500">Hotels</a>
-          <a href="#" className="hover:text-yellow-500">Flights</a>
-          <a href="#" className="hover:text-yellow-500">Bookings</a>
-
-          <button className="font-bold hover:text-yellow-500">Login</button>
-          <button className="border px-4 py-1 rounded font-bold hover:bg-yellow-500 hover:text-white transition">
-            Sign up
-          </button>
-        </div>
-      )}
 
     </header>
   );
